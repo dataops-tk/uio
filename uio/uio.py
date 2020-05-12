f5626eb0025c36630a4d7fc4045b9c9f0da9b63e
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-05-11 23:11:29 -0700

    logger rename fix

--- uio/uio.py
+++ uio/uio.py
@@ -370,9 +370,9 @@ def upload_file(local_path, remote_path):
     If you are not sure if the source file is local, use `copy_file` instead.
     """
     if not is_local(local_path):
-        _logging.warning(f"Attempting to upload a non-local file '${local_path}'.")
+        _LOGGER.warning(f"Attempting to upload a non-local file '${local_path}'.")
     elif not file_exists(local_path):
-        _logging.warning(f"Attempting to upload a non-existent file '${local_path}'.")
+        _LOGGER.warning(f"Attempting to upload a non-existent file '${local_path}'.")
 
     fn = _pick_cloud_function(
         remote_path,
