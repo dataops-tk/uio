4d43a5ebbb742236ab16871125be92e38bb87645
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-05-13 18:54:55 -0700

    add test

---
+++ uio/tests/test_io.py
@@ -0,0 +1,22 @@
+import unittest
+import xmlrunner
+import os
+
+PARENT_FOLDER = os.path.dirname(os.path.realpath(__file__))
+PROJECT_ROOT = PARENT_FOLDER
+
+import uio
+
+
+class MyTest(unittest.TestCase):
+    def test_dummy(self):
+        assert True
+
+    def test_file_exists(self):
+        assert uio.file_exists("uio/__init__.py")
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+    # x = MyTest()
+    # x.test_safe_writes()
